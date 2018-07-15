dfe0194015cc83998a1c605d44d47214a2b21bf8
ellarobertson <ella.a.robertson@gmail.com>
2018-07-15 14:13:18 -0500

    display static goal cells in table view

---
+++ GoalPost/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  GoalPost
+//
+//  Created by Ella Robertson on 7/15/18.
+//  Copyright © 2018 Ella Robertson. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
