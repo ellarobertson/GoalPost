58f3be6daa6b929d75c8ee023fc7e3a6b8586c71
ellarobertson <ella.a.robertson@gmail.com>
2018-07-15 13:17:40 -0500

    created GoalCell

---
+++ GoalPost/View/GoalCell.swift
@@ -0,0 +1,32 @@
+//
+//  GoalCell.swift
+//  GoalPost
+//
+//  Created by Ella Robertson on 7/15/18.
+//  Copyright © 2018 Ella Robertson. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    //Outlets
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
