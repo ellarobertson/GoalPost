5ee7293fc245a0bdcc42f4dd5fdb07f10f65c9ca
ellarobertson <ella.a.robertson@gmail.com>
2018-07-15 16:16:52 -0500

    CreateGoalVC created

---
+++ GoalPost/Controller/CreateGoalVC.swift
@@ -0,0 +1,41 @@
+//
+//  CreateGoalVC.swift
+//  GoalPost
+//
+//  Created by Ella Robertson on 7/15/18.
+//  Copyright © 2018 Ella Robertson. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBttn: UIButton!
+    @IBOutlet weak var longTermBttn: UIButton!
+    @IBOutlet weak var nextBttn: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func nextBttnWasPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func shortTermBttnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermBttnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBttnWasPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
